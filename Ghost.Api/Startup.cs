3000d35e10d0800b894b1eddfa89c78fe67896fb
HADES <1595375648@qq.com>
2021-01-05 21:30:46 +0800

    JWT授权

--- Ghost.Api/Startup.cs
+++ Ghost.Api/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,7 +38,38 @@ namespace Ghost.Api
                     //Contact = new OpenApiContact { Name = "Ghost", Email = "Blog.Core@xxx.com", Url = new Uri("https://www.jianshu.com/u/94102b59cc2a") },
                     //License = new OpenApiLicense { Name = "Ghost", Url = new Uri("https://www.jianshu.com/u/94102b59cc2a") }
                 });
-                c.IncludeXmlComments("Ghost.Api.xml", true);//注释提示
+
+                // 开启加权小锁
+                c.OperationFilter<AddResponseHeadersFilter>();
+                c.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
+
+                // 在header中添加token，传递到后台
+                c.OperationFilter<SecurityRequirementsOperationFilter>();
+                c.IncludeXmlComments("Ghost.Api.xml", true);//注释提示 
+                //接入identityserver4
+                //c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                //{
+                //    Type = SecuritySchemeType.OAuth2,
+                //    Flows = new OpenApiOAuthFlows
+                //    {
+                //        Implicit = new OpenApiOAuthFlow
+                //        {
+                //            AuthorizationUrl = new Uri($"{Appsettings.app(new string[] { "Startup", "IdentityServer4", "AuthorizationUrl" })}/connect/authorize"),
+                //            Scopes = new Dictionary<string, string> {
+                //                { "blog.core.api","ApiResource id" }
+                //            }
+                //        }
+                //    }
+                //});
+
+                // Jwt Bearer 认证，必须是 oauth2
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "JWT授权(数据将在请求头中进行传输) 直接在下框中输入Bearer {token}（注意两者之间是一个空格）\"",
+                    Name = "Authorization",//jwt默认的参数名称
+                    In = ParameterLocation.Header,//jwt默认存放Authorization信息的位置(请求头中)
+                    Type = SecuritySchemeType.ApiKey
+                });
             });
         }
 
